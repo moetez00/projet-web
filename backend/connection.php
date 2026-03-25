194feebd8bbadba8ddd3a80f5f2d6bfa0afe6190
Taz <moetezzouari1@gmail.com>
2026-03-25 15:51:40 +0100

    Add connection to database file

---
+++ backend/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once realpath(__DIR__ . "/vendor/autoload.php");
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+$dotenv->required(['DB_HOST', 'DB_NAME', 'DB_USERNAME', 'DB_PASSWORD']);
+
+$servername =$_ENV['DB_HOST'];
+$dbnae=$_ENV['DB_NAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+
+$connection = new mysqli($servername, $username,$password,$dbname);
+if ($connection->connect_error) { die("connection error : " . $connection->connect_error);}
+?>
+
