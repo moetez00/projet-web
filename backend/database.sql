3d6eec5ee74d42125b5faf53869a2841eba6a593
Zeineb Triki <76732158+zeineb12345@users.noreply.github.com>
2026-03-25 14:41:35 +0100

    fix database

--- backend/database.sql
+++ backend/database.sql
@@ -3,47 +3,48 @@ use insatpulse_db;
 
 CREATE TABLE User(
 	id INT PRIMARY KEY auto_increment,
-    username VARCHAR(30),
-    email VARCHAR(50);
-	password VARCHAR(50)
+    username VARCHAR(30) UNIQUE,
+    email VARCHAR(50) UNIQUE,
+	password VARCHAR(50),
+    registerDate Date
 );
 CREATE TABLE Club(
     id INT PRIMARY KEY,
-    clubName VARCHAR(50)
-    description VARCHAR(700),
-    logoURL VARCHAR(400),
-    creationDate Date,
+    clubName VARCHAR(50),
+    description VARCHAR(700) default NULL,
+    logoURL VARCHAR(400) default NULL,
     FOREIGN KEY (id) REFERENCES User(id)
 );
 CREATE TABLE Student(
     id INT PRIMARY KEY,
-    fullName VARCHAR(30);
-    registerDate Date;
+    fullName VARCHAR(30),
     FOREIGN KEY (id) REFERENCES User(id)
 );
-CREATE TABLE Suivre(
+CREATE TABLE Follow(
     id_Student INT PRIMARY KEY,
     id_Club INT PRIMARY KEY,
-    date Date,
+    dateFollow Date,
     FOREIGN KEY(id_Student) REFERENCES Student(id),
-    FOREIGN KEY(id_Club) REFERENCES Club(id),
+    FOREIGN KEY(id_Club) REFERENCES Club(id)
 );
 
 CREATE TABLE EVENT(
     id INT PRIMARY KEY auto_increment,
-    title VARCHAR(40);
+    id_Club INT,
+    title VARCHAR(40),
     description VARCHAR(700),
     startDate Date,
     endDate Date,
-    location VARCHAR(60),
-    imageURL VARCHAR(60)
+    loc VARCHAR(60),
+    imageURL VARCHAR(60),
+    FOREIGN KEY(id_Club) REFERENCES Club(id)
 );
 
 CREATE TABLE LIKE(
     id_Student INT PRIMARY KEY,
     id_Club INT PRIMARY KEY,
-    date Date,
+    dateLike Date,
     FOREIGN KEY(id_Student) REFERENCES Student(id),
-    FOREIGN KEY(id_Club) REFERENCES Club(id),
+    FOREIGN KEY(id_Club) REFERENCES Club(id)
 
 );
