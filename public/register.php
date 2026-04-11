522351ab050a646439d34e0a9670a4e245a65c4a
Taz <moetezzouari1@gmail.com>
2026-04-11 16:38:45 +0100

    Minor fix

--- public/register.php
+++ public/register.php
@@ -8,7 +8,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
 
     <!-- Bootstrap CSS v5.2.1 -->
-    <link href="/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" />
+    <!-- link href="/node_modules/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet" / -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+
     <link rel="stylesheet" href="assets/css/choice.css">
 
 </head>
@@ -70,10 +72,12 @@
 
     </main>
 
-    <!-- Bootstrap JavaScript Libraries -->
+    <!-- Bootstrap JavaScript Libraries 
     <script src="/node_modules/bootstrap/dist/umd/popper.min.js"></script>
 
-    <script src="//node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
+    <script src="//node_modules/bootstrap/dist/js/bootstrap.min.js"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
 </body>
 
 </html>
